359fec61278591f2f369fe292bd906578a9a1a00
ohde-sg <ohde@socialgrid.co.jp>
2015-12-17 03:00:23 +0900

    Initial Commit

---
+++ SwiftCrypton/SwiftCrypton.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCrypton.h
+//  SwiftCrypton
+//
+//  Created by 大出喜之 on 2015/12/17.
+//  Copyright © 2015年 yoshiyuki ohde. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftCrypton.
+FOUNDATION_EXPORT double SwiftCryptonVersionNumber;
+
+//! Project version string for SwiftCrypton.
+FOUNDATION_EXPORT const unsigned char SwiftCryptonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCrypton/PublicHeader.h>
+
+
